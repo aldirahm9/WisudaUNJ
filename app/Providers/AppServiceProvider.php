85d4f0dc44934e34d9f533edeb1e4e4e801bd9cb
Erik Santiago <eriksantiago.science@gmail.com>
2020-10-16 11:34:14 +0700

    Update procfile ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register(UrlGenerator $url)
+    public function register()
     {
         if (env('APP_ENV') === 'production') {
             $this->app['request']->server->set('HTTPS', true);

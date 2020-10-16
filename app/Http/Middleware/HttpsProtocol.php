d89f50e60ac2ddd04cb0e0c67f9caec2a9e23283
Erik Santiago <eriksantiago.science@gmail.com>
2020-10-16 09:19:58 +0700

    setting middleware http1

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && $this->app->environment('production')) {
+        if (!$request->secure() && env('APP_ENV') == 'production') {
             return redirect()->secure($request->getRequestUri());
         }
 

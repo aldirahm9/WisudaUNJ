d614bacd503e6a9c433adc59617ea6c55e3a63ac
Muhammad Fathan Qoriiba <mfathanqoriiba@gmail.com>
2020-10-17 12:09:12 +0700

    slot admin controller

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pendaftaran;
 use App\Models\Fakultas;
+use App\Models\Slot;
 
 class adminController extends Controller
 {
     public function dashboard()
     {
+        $slot = Slot::all();
         $fakultas = Fakultas::all();
         $pendaftaran = Pendaftaran::all();
-        return view('dashboard', ['pendaftaran' => $pendaftaran, 'fakultas' => $fakultas]);
+        return view('dashboard', ['pendaftaran' => $pendaftaran, 'fakultas' => $fakultas, 'slot' => $slot]);
     }
 }

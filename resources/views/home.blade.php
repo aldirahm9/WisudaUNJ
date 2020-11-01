abe67fbc5938b87bb458fd0737fab62b0a5edd1a
Muhammad Fathan Qoriiba <mfathanqoriiba@gmail.com>
2020-11-01 17:47:35 +0700

    link foto wisuda

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -284,7 +284,13 @@
 
         <!-- Content Section -->
         <div class="bg-light">
-            <div class="container space-2">
+            <div class="container space-1">
+                <div class="alert alert-success" role="alert">
+                    Foto Wisuda Anda dapat dilihat
+                    <a href="https://drive.google.com/drive/folders/1xV8XvGr1VvlikqFMtzSFe5Kc-s31AGog?usp=sharing"  target="blank">
+                        di sini!
+                    </a>
+                </div>
                 <div class="card card-frame card-frame-highlighted mb-6">
                     <div class="card-header py-4 px-0 mx-4">
                         <!-- Title -->

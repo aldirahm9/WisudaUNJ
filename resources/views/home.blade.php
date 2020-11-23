8cdec7cc90c619a8c038f78291874e924e446d7e
Aldi Rahmansyah <aldi.rahmansyah99@gmail.com>
2020-11-23 09:16:11 +0700

    fix tanggal satuan gantitanggal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -628,7 +628,7 @@
                 if(+new Date(date.format('M/D/YYYY')) < now)
                 return true
                 // return (date.day() == 0 || date.day() == 6 || +new Date(date.format('M/D/YYYY')) <= now|| +new Date(date.format('M/D/YYYY')) == +new Date('10/30/2020')|| +new Date(date.format('M/D/YYYY')) == +new Date('10/29/2020'));
-                if(arrValidDates.includes(date.format('YYYY-M-D')))
+                if(arrValidDates.includes(date.format('YYYY-MM-DD')))
                 return false
                 return true
             },
